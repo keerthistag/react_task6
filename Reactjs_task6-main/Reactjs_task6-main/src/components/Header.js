2cd49e7c93667d84239e629825f1924e322a20fd
chikkala keerthi <134956823+keerthistag@users.noreply.github.com>
2023-10-23 09:43:12 +0530

    Add files via upload

---
+++ Reactjs_task6-main/Reactjs_task6-main/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Shaik Rabhiya</h1>
+      <h2>Web Developer</h2>
+      <h3>studying in vit ap university</h3>
+      
+    </div>
+  );
+}
+
+export default Header;
